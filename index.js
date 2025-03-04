e1143d7def24a72dbc37b62a7d0e1284208986f7
freddy <Frederik.Folkers@web.de>
2025-03-04 13:59:32 +0100

    improved indexpage

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 let searchInput = document.getElementById("searchInput");
 let searchResult = document.getElementById("searchResults");
+let fuse;
+let allImg;
 fetch("./annoList.json").then((res) => res.json().then((a) => {
-  let fuse = new Fuse(a, {
+  allImg = a;
+  fuse = new Fuse(a, {
+    shouldSort: true,
     keys: [
       "id",
       "name",
       "keywords"
     ]
   });
-  update_search_list(fuse.search(searchInput.value));
+  update_search_list(allImg);
 }));
+searchInput.addEventListener("input", () => {
+  // if you search for nothing all should be shown
+  if(searchInput.value == "") {
+    update_search_list(allImg);
+  } else {
+    update_search_list(fuse.search(searchInput.value).map((a) => a.item));
+  }
+
+});
 
 function update_search_list(searchList) {
   searchResult.innerHTML = "";
-  searchList.forEach((searchItem) => {
-    let item = searchItem.item;
+  searchList.forEach((item) => {
+    // create each Element
     let elem = document.createElement("div");
     elem.className = "item";
 
+    let imgDiv = document.createElement("div");
+    imgDiv.className = "imageDiv";
     let img = document.createElement("img");
     img.src = item.path + "/thumb.jpg";
-    elem.appendChild(img);
+    imgDiv.appendChild(img);
+    elem.appendChild(imgDiv);
 
     let elemInfo = document.createElement("table");
     elemInfo.className = "itemInfo";
@@ -38,14 +54,18 @@ function update_search_list(searchList) {
     elemInfo.appendChild(idRow);
     elem.appendChild(elemInfo);
 
+    // when clicked go to pic
+    elem.addEventListener("click", () => {
+      if(window.location.search == "") {
+        window.location.href = "./viewer.html?image=" + item.id;
+      } else {
+        window.location.href = "./viewer.html" + window.location.search + "&image=" + item.id;
+      }
+    });
     searchResult.appendChild(elem);
   });
 }
 
 let itemList = document.getElementsByClassName("imageItem");
 for( let i of itemList) {
-  let imgName = i.attributes.getNamedItem("imgName").value;
-  i.addEventListener("click", () => {
-    window.location.href = "./viewer.html?image=" + imgName;
-  });
 }
